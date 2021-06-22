36d4938aee64c7cc3c9078dc93913bd372932805
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-22 20:27:37 +0530

    fixed a bug in registration and login

--- authentication/urls.py
+++ authentication/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-   path('', views.register, name='register')
+   
 ]
 
--- liveClass/urls.py
+++ liveClass/urls.py
@@ -20,7 +20,7 @@ from authentication import views as authentication_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('liveclass_api.urls')),
+    path('register/', authentication_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='authentication/logout.html'), name='logout'),
   
