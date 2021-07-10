0671872de65d287f85a631e7b367412f5fc79beb
dhruv354 <dhruvsin@iitk.ac.in>
2021-07-10 11:52:05 +0530

    removed fraft thing from doubtclass view

--- liveclass_api/views.py
+++ liveclass_api/views.py
@@ -255,7 +255,7 @@ def RegisterClass(request):
 class ListDrafts(mixins.ListModelMixin, generics.GenericAPIView):
 
     serializer_class = serializers.LiveClass_details_serializer
-    queryset = models.LiveClass_details.objects.all()
+    queryset = models.LiveClass_details.objects.filter(isDraft=True)
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -268,7 +268,7 @@ class ListDrafts(mixins.ListModelMixin, generics.GenericAPIView):
 class DraftClassId(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin,  mixins.UpdateModelMixin, generics.GenericAPIView):
 
     serializer_class = serializers.LiveClass_details_serializer
-    queryset = models.LiveClass_details.objects.all()
+    queryset = models.LiveClass_details.objects.filter(isDraft=True)
     permission_classes = [IsAuthenticated]
     lookup_field = 'id'
 
@@ -344,7 +344,7 @@ class DoubtDraftClassId(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins
 class DoubtClass( mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
 
     serializer_class = serializers.DoubtClass_serializer
-    queryset = models.DoubtClasses.objects.filter(isDraft=False)
+    queryset = models.DoubtClasses.objects.all()
     permission_classes = [IsAuthenticated]
     def get(self, request):
         return self.list(request)
@@ -359,7 +359,7 @@ class DoubtClass( mixins.ListModelMixin, mixins.CreateModelMixin, generics.Gener
 #view for a particular doubt class object
 class DoubtClassId(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     serializer_class = serializers.DoubtClass_serializer
-    queryset = models.DoubtClasses.objects.filter(isDraft=False)
+    queryset = models.DoubtClasses.objects.all()
     permission_classes = [IsAuthenticated]
     lookup_field = 'id'
 
